6e4be5b77d48f363131de2e8d43e27475670c236
dsj47 <dsj47@cornell.edu>
2025-02-12 16:37:05 -0800

    package/confix fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-const productionBase = process.env.NODE_ENV === 'production' ? '/web_v2/' : '/';
+// const productionBase = process.env.NODE_ENV === 'production' ? '/web_v2/' : '/';
 
 export default defineConfig({
-  base: productionBase, // Adjust "repo-name" to match your GitHub repository name
+  base: './', // 
   plugins: [react()],
   build: {
-    outDir: 'dist', // Confirm output directory
-    // other build options
+    outDir: 'dist', 
   }
 });
