5662f73b914f95fd7411a3f87eede185c4b82624
dsj47 <dsj47@cornell.edu>
2025-03-03 20:32:58 -0800

    edit

--- src/pages/Cover/Cover.tsx
+++ src/pages/Cover/Cover.tsx
@@ -37,7 +37,7 @@ const Cover = () => {
             where you can learn a little bit more about me.
           </div>
 
-          <svg className="block sm:block lg:block arrows w-6 h-6 md:w-8 md:h-8 text-line text-line-9">
+          <svg className="hidden lg:block arrows w-6 h-6 md:w-8 md:h-8 text-line text-line-9">
             <path
               className="a1"
               d="M0 0 L30 32 L60 0"
